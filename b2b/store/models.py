5e62d78e174408a5728ca0510058dc982dac959f
Monis Momin <monismomin123@gmail.com>
2024-11-12 18:32:38 +0530

    Registration page problem

---
+++ b2b/store/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+import datetime
+# Models (Database)
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    # To change the naming of Models(Not that imp)
+    class Meta:
+        verbose_name_plural='categories'
+
+
+
+class Customer(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    phone=models.CharField(max_length=50)
+    email=models.EmailField(max_length=100)
+    password=models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(default=0,decimal_places=2,max_digits=9)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+    description=models.CharField(max_length=250,default='',blank=True,null=True)
+    image=models.ImageField(upload_to='upload/product/')
+
+    # Add Sale Tag Stuff
+    on_sale=models.BooleanField(default=False)
+    sale_price=models.DecimalField(default=0,decimal_places=2,max_digits=6)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    quantity=models.ImageField(default=1)
+    address=models.CharField(max_length=100,default='',blank=True)
+    phone=models.CharField(max_length=50,default='',blank=True)
+    date=models.DateField(default=datetime.datetime.today)
+    status=models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.product}"
+
+
+
+# Need to add
+# Store
+# Buyer
+# Seller
